bce1661b5b6c0a2abced0a5ad2793811d107159a
Rajesh18rj <rajeshkumar18rj@gmail.com>
2024-11-08 14:00:49 +0530

    1-2 newTest - PestContactsTest

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -1,16 +1,5 @@
 <?php
 
-namespace Tests\Unit;
-
-use PHPUnit\Framework\TestCase;
-
-class ExampleTest extends TestCase
-{
-    /**
-     * A basic test example.
-     */
-    public function test_example(): void
-    {
-        $this->assertTrue(true);
-    }
-}
+it('example', function(){
+    $this->assertTrue(true);
+});
